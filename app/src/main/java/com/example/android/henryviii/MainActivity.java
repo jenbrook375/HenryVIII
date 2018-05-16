99b3cd875b89f34345838b80ba7f544617c2181c
JenBrook <jenbrook375@hotmail.com>
2018-05-16 09:00:18 -0700

    clean up for submittal
    many changes, mostly to styles, dimen, xml files. batch import of drawables

--- app/src/main/java/com/example/android/henryviii/MainActivity.java
+++ app/src/main/java/com/example/android/henryviii/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
@@ -28,65 +30,90 @@ public class MainActivity extends AppCompatActivity {
      */
     public void submit(View view) {
 
-        /** Question 1 (england)*/
-        RadioButton england;
-        england = findViewById(R.id.england);
-        england.isChecked();
-        if (england.isChecked()) {
-            score = score + 1;
-            Log.v("MyActivity", "score" + score);
-        }
+        /** Question 1 correct answer field England*/
+        EditText text = findViewById(R.id.question1);
+        String answer = text.getText().toString();
 
-        /** Question 2 (cspain)*/
-        RadioButton cspain;
-        cspain = findViewById(R.id.cspain);
-        cspain.isChecked();
-        if (cspain.isChecked()) {
+        if (answer.equals("ENGLAND")) {
             score = score + 1;
         }
 
-        /** Question 3 (elizabeth)*/
-        RadioButton elizabeth;
-        elizabeth = findViewById(R.id.elizabeth);
-        elizabeth.isChecked();
-        if (elizabeth.isChecked()) {
+        /** Question 2 correct answer (cspain)*/
+        RadioButton answer_2;
+        answer_2 = findViewById(R.id.cspain);
+        answer_2.isChecked();
+        if (answer_2.isChecked()) {
             score = score + 1;
         }
 
-        /** Question 4 (birth)*/
-        RadioButton birth;
-        birth = findViewById(R.id.birth);
-        birth.isChecked();
-        if (birth.isChecked()) {
+        /** Question 3 correct answer(elizabeth)*/
+        RadioButton answer_3;
+        answer_3 = findViewById(R.id.elizabeth);
+        answer_3.isChecked();
+        if (answer_3.isChecked()) {
             score = score + 1;
         }
 
-        /** Question 5 (friends)*/
-        RadioButton friends;
-        friends = findViewById(R.id.friends);
-        friends.isChecked();
-        if (friends.isChecked()) {
+        /** Question 4 correct answer(birth)*/
+        RadioButton answer_4;
+        answer_4 = findViewById(R.id.birth);
+        answer_4.isChecked();
+        if (answer_4.isChecked()) {
             score = score + 1;
         }
 
-        /** Question 6 (fornication)*/
-        RadioButton fornication;
-        fornication = findViewById(R.id.fornication);
-        fornication.isChecked();
-        if (fornication.isChecked()) {
+        /** Question 5 correct answer(friends)*/
+        RadioButton answer_5;
+        answer_5 = findViewById(R.id.friends);
+        answer_5.isChecked();
+        if (answer_5.isChecked()) {
             score = score + 1;
         }
 
-        /** Question 7 (outlived)*/
-        RadioButton a7;
-        a7 = findViewById(R.id.a7);
-        a7.isChecked();
-        if (a7.isChecked()) {
+        /** Question 6 correct answer(fornication)*/
+        RadioButton answer_6;
+        answer_6 = findViewById(R.id.fornication);
+        answer_6.isChecked();
+        if (answer_6.isChecked()) {
             score = score + 1;
         }
 
+        /** Question 7 correct answer(outlived)*/
+        RadioButton answer_7;
+        answer_7 = findViewById(R.id.outlived);
+        answer_7.isChecked();
+        if (answer_7.isChecked()) {
+            score = score + 1;
+        }
 
-          if (score == 7) {
+        // extra credit quest 1 checkbox
+        CheckBox  checkbox_1= (CheckBox) findViewById(R.id.chkbx_1);
+        boolean  excr_1= checkbox_1.isChecked();
+
+        // adds 1 point if extra credit question is checked
+        if (excr_1){
+            score = score + 1;
+        }
+
+        // extra credit quest 2 checkbox
+        CheckBox checkbox_2 = (CheckBox) findViewById(R.id.chkbx_2);
+        boolean excr_2 = checkbox_2.isChecked();
+
+        // adds 1 point if extra credit question is checked
+        if (excr_2) {
+            score = score + 1;
+        }
+
+        // extra credit quest 3 checkbox
+        CheckBox checkbox_3 = (CheckBox) findViewById(R.id.chkbx_3);
+        boolean excr_3 = checkbox_3.isChecked();
+
+        // adds 1 point if extra credit question is checked
+        if (excr_3) {
+            score = score + 1;
+        }
+
+          if (score >= 7) {
             Toast.makeText(this, R.string.perfect,Toast.LENGTH_LONG).show();
         } else if (score == 6) {
             Toast.makeText(this, R.string.history, Toast.LENGTH_LONG).show();
